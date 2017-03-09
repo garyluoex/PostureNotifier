2b51d8d9d9469ba54bd9874950665e67ae955c1d
Weijia Luo <wluo7@illinois.edu>
2017-03-08 20:27:07 -0600

    Basic testing usb4java library

--- src/main/java/com/garyluoex/project/App.java
+++ src/main/java/com/garyluoex/project/App.java
@@ -1,13 +1,24 @@
 package com.garyluoex.project;
 
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+import javax.usb.UsbDevice;
+import javax.usb.UsbDisconnectedException;
+import javax.usb.UsbException;
+import javax.usb.UsbHostManager;
+import javax.usb.UsbHub;
+import javax.usb.UsbServices;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) throws UsbException, UnsupportedEncodingException, UsbDisconnectedException {
+		UsbServices services = UsbHostManager.getUsbServices();
+		UsbHub rootHub = services.getRootUsbHub();
+		List<UsbDevice> devices = rootHub.getAttachedUsbDevices();
+		System.out.println(devices.get(0).getUsbDeviceDescriptor().idVendor());
+	}
 }
